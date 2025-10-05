74f0fe44baa3e6f378b4e18f3ad42fe76f21f8f3
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-10-05 14:44:38 +0200

    Add presets

---
+++ src/constants/presets.js
@@ -0,0 +1,226 @@
+/**
+ * Popular color palette presets
+ */
+
+export const COLOR_PRESETS = [
+    {
+        name: 'Dracula',
+        author: 'Dracula Theme',
+        colors: [
+            '#282a36', // background
+            '#ff5555', // red
+            '#50fa7b', // green
+            '#f1fa8c', // yellow
+            '#bd93f9', // purple
+            '#ff79c6', // pink
+            '#8be9fd', // cyan
+            '#f8f8f2', // foreground
+            '#6272a4', // comment
+            '#ff6e6e', // bright red
+            '#69ff94', // bright green
+            '#ffffa5', // bright yellow
+            '#d6acff', // bright purple
+            '#ff92df', // bright pink
+            '#a4ffff', // bright cyan
+            '#ffffff', // bright white
+        ]
+    },
+    {
+        name: 'Nord',
+        author: 'Arctic Bluish',
+        colors: [
+            '#2e3440', // background
+            '#bf616a', // red
+            '#a3be8c', // green
+            '#ebcb8b', // yellow
+            '#81a1c1', // blue
+            '#b48ead', // magenta
+            '#88c0d0', // cyan
+            '#e5e9f0', // foreground
+            '#4c566a', // bright black
+            '#d08770', // bright red
+            '#a3be8c', // bright green
+            '#ebcb8b', // bright yellow
+            '#81a1c1', // bright blue
+            '#b48ead', // bright magenta
+            '#8fbcbb', // bright cyan
+            '#eceff4', // bright white
+        ]
+    },
+    {
+        name: 'Gruvbox Dark',
+        author: 'Retro Groove',
+        colors: [
+            '#282828', // background
+            '#cc241d', // red
+            '#98971a', // green
+            '#d79921', // yellow
+            '#458588', // blue
+            '#b16286', // magenta
+            '#689d6a', // cyan
+            '#ebdbb2', // foreground
+            '#928374', // bright black
+            '#fb4934', // bright red
+            '#b8bb26', // bright green
+            '#fabd2f', // bright yellow
+            '#83a598', // bright blue
+            '#d3869b', // bright magenta
+            '#8ec07c', // bright cyan
+            '#fbf1c7', // bright white
+        ]
+    },
+    {
+        name: 'Solarized Dark',
+        author: 'Precision Colors',
+        colors: [
+            '#002b36', // background
+            '#dc322f', // red
+            '#859900', // green
+            '#b58900', // yellow
+            '#268bd2', // blue
+            '#d33682', // magenta
+            '#2aa198', // cyan
+            '#839496', // foreground
+            '#073642', // bright black
+            '#cb4b16', // bright red
+            '#586e75', // bright green
+            '#657b83', // bright yellow
+            '#839496', // bright blue
+            '#6c71c4', // bright magenta
+            '#93a1a1', // bright cyan
+            '#fdf6e3', // bright white
+        ]
+    },
+    {
+        name: 'Tokyo Night',
+        author: 'Night Coding',
+        colors: [
+            '#1a1b26', // background
+            '#f7768e', // red
+            '#9ece6a', // green
+            '#e0af68', // yellow
+            '#7aa2f7', // blue
+            '#bb9af7', // magenta
+            '#7dcfff', // cyan
+            '#a9b1d6', // foreground
+            '#414868', // bright black
+            '#f7768e', // bright red
+            '#9ece6a', // bright green
+            '#e0af68', // bright yellow
+            '#7aa2f7', // bright blue
+            '#bb9af7', // bright magenta
+            '#7dcfff', // bright cyan
+            '#c0caf5', // bright white
+        ]
+    },
+    {
+        name: 'Catppuccin Mocha',
+        author: 'Soothing Pastel',
+        colors: [
+            '#1e1e2e', // background
+            '#f38ba8', // red
+            '#a6e3a1', // green
+            '#f9e2af', // yellow
+            '#89b4fa', // blue
+            '#cba6f7', // magenta
+            '#94e2d5', // cyan
+            '#cdd6f4', // foreground
+            '#45475a', // bright black
+            '#f38ba8', // bright red
+            '#a6e3a1', // bright green
+            '#f9e2af', // bright yellow
+            '#89b4fa', // bright blue
+            '#cba6f7', // bright magenta
+            '#94e2d5', // bright cyan
+            '#bac2de', // bright white
+        ]
+    },
+    {
+        name: 'One Dark',
+        author: 'Atom Inspired',
+        colors: [
+            '#282c34', // background
+            '#e06c75', // red
+            '#98c379', // green
+            '#e5c07b', // yellow
+            '#61afef', // blue
+            '#c678dd', // magenta
+            '#56b6c2', // cyan
+            '#abb2bf', // foreground
+            '#5c6370', // bright black
+            '#e06c75', // bright red
+            '#98c379', // bright green
+            '#e5c07b', // bright yellow
+            '#61afef', // bright blue
+            '#c678dd', // bright magenta
+            '#56b6c2', // bright cyan
+            '#ffffff', // bright white
+        ]
+    },
+    {
+        name: 'Monokai Pro',
+        author: 'Sublime Classic',
+        colors: [
+            '#2d2a2e', // background
+            '#ff6188', // red
+            '#a9dc76', // green
+            '#ffd866', // yellow
+            '#fc9867', // orange/blue
+            '#ab9df2', // purple
+            '#78dce8', // cyan
+            '#fcfcfa', // foreground
+            '#727072', // bright black
+            '#ff6188', // bright red
+            '#a9dc76', // bright green
+            '#ffd866', // bright yellow
+            '#fc9867', // bright orange
+            '#ab9df2', // bright purple
+            '#78dce8', // bright cyan
+            '#fcfcfa', // bright white
+        ]
+    },
+    {
+        name: 'Palenight',
+        author: 'Material Theme',
+        colors: [
+            '#292d3e', // background
+            '#f07178', // red
+            '#c3e88d', // green
+            '#ffcb6b', // yellow
+            '#82aaff', // blue
+            '#c792ea', // magenta
+            '#89ddff', // cyan
+            '#bfc7d5', // foreground
+            '#676e95', // bright black
+            '#f07178', // bright red
+            '#c3e88d', // bright green
+            '#ffcb6b', // bright yellow
+            '#82aaff', // bright blue
+            '#c792ea', // bright magenta
+            '#89ddff', // bright cyan
+            '#ffffff', // bright white
+        ]
+    },
+    {
+        name: 'Rose Pine',
+        author: 'Rosé Pine',
+        colors: [
+            '#191724', // background
+            '#eb6f92', // red
+            '#9ccfd8', // green
+            '#f6c177', // yellow
+            '#31748f', // blue
+            '#c4a7e7', // magenta
+            '#ebbcba', // cyan
+            '#e0def4', // foreground
+            '#26233a', // bright black
+            '#eb6f92', // bright red
+            '#9ccfd8', // bright green
+            '#f6c177', // bright yellow
+            '#31748f', // bright blue
+            '#c4a7e7', // bright magenta
+            '#ebbcba', // bright cyan
+            '#e0def4', // bright white
+        ]
+    },
+];
