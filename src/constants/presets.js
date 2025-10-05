854de055252cd910b0138218584c6ff9a7733996
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-10-05 20:37:41 +0200

    Add more well-known themes

--- src/constants/presets.js
+++ src/constants/presets.js
@@ -223,4 +223,423 @@ export const COLOR_PRESETS = [
             '#e0def4', // bright white
         ]
     },
+    {
+        name: 'Everforest',
+        author: 'Nature Inspired',
+        colors: [
+            '#2b3339', // background
+            '#e67e80', // red
+            '#a7c080', // green
+            '#dbbc7f', // yellow
+            '#7fbbb3', // blue
+            '#d699b6', // magenta
+            '#83c092', // cyan
+            '#d3c6aa', // foreground
+            '#475258', // bright black
+            '#e67e80', // bright red
+            '#a7c080', // bright green
+            '#dbbc7f', // bright yellow
+            '#7fbbb3', // bright blue
+            '#d699b6', // bright magenta
+            '#83c092', // bright cyan
+            '#d3c6aa', // bright white
+        ]
+    },
+    {
+        name: 'Kanagawa',
+        author: 'The Great Wave',
+        colors: [
+            '#1f1f28', // background
+            '#c34043', // red
+            '#76946a', // green
+            '#c0a36e', // yellow
+            '#7e9cd8', // blue
+            '#957fb8', // magenta
+            '#6a9589', // cyan
+            '#dcd7ba', // foreground
+            '#363646', // bright black
+            '#e82424', // bright red
+            '#98bb6c', // bright green
+            '#e6c384', // bright yellow
+            '#7fb4ca', // bright blue
+            '#938aa9', // bright magenta
+            '#7aa89f', // bright cyan
+            '#c8c093', // bright white
+        ]
+    },
+    {
+        name: 'Nightfox',
+        author: 'Night Coding',
+        colors: [
+            '#192330', // background
+            '#c94f6d', // red
+            '#81b29a', // green
+            '#dbc074', // yellow
+            '#719cd6', // blue
+            '#9d79d6', // magenta
+            '#63cdcf', // cyan
+            '#cdcecf', // foreground
+            '#526176', // bright black
+            '#c94f6d', // bright red
+            '#81b29a', // bright green
+            '#dbc074', // bright yellow
+            '#719cd6', // bright blue
+            '#9d79d6', // bright magenta
+            '#63cdcf', // bright cyan
+            '#cdcecf', // bright white
+        ]
+    },
+    {
+        name: 'Ayu Mirage',
+        author: 'Modern Elegance',
+        colors: [
+            '#1f2430', // background
+            '#f28779', // red
+            '#d5ff80', // green
+            '#ffd173', // yellow
+            '#73d0ff', // blue
+            '#dfbfff', // magenta
+            '#95e6cb', // cyan
+            '#cbccc6', // foreground
+            '#707a8c', // bright black
+            '#f28779', // bright red
+            '#d5ff80', // bright green
+            '#ffd173', // bright yellow
+            '#73d0ff', // bright blue
+            '#dfbfff', // bright magenta
+            '#95e6cb', // bright cyan
+            '#cbccc6', // bright white
+        ]
+    },
+    {
+        name: 'Oceanic Next',
+        author: 'Ocean Inspired',
+        colors: [
+            '#1b2b34', // background
+            '#ec5f67', // red
+            '#99c794', // green
+            '#fac863', // yellow
+            '#6699cc', // blue
+            '#c594c5', // magenta
+            '#5fb3b3', // cyan
+            '#d8dee9', // foreground
+            '#65737e', // bright black
+            '#ec5f67', // bright red
+            '#99c794', // bright green
+            '#fac863', // bright yellow
+            '#6699cc', // bright blue
+            '#c594c5', // bright magenta
+            '#5fb3b3', // bright cyan
+            '#d8dee9', // bright white
+        ]
+    },
+    {
+        name: 'Horizon',
+        author: 'Red Sunset',
+        colors: [
+            '#1c1e26', // background
+            '#e95678', // red
+            '#29d398', // green
+            '#fab795', // yellow
+            '#26bbd9', // blue
+            '#ee64ac', // magenta
+            '#59e3e3', // cyan
+            '#fadad1', // foreground
+            '#6c6f93', // bright black
+            '#ec6a88', // bright red
+            '#3fdaa4', // bright green
+            '#fbc3a7', // bright yellow
+            '#3fc4de', // bright blue
+            '#f075b5', // bright magenta
+            '#6be4e6', // bright cyan
+            '#fadad1', // bright white
+        ]
+    },
+    {
+        name: 'Andromeda',
+        author: 'Sci-Fi Dreams',
+        colors: [
+            '#23262e', // background
+            '#ee5d43', // red
+            '#96e072', // green
+            '#ffe66d', // yellow
+            '#00a9f9', // blue
+            '#f92aad', // magenta
+            '#4dd9d7', // cyan
+            '#d5ced9', // foreground
+            '#666b81', // bright black
+            '#ee5d43', // bright red
+            '#96e072', // bright green
+            '#ffe66d', // bright yellow
+            '#00a9f9', // bright blue
+            '#f92aad', // bright magenta
+            '#4dd9d7', // bright cyan
+            '#d5ced9', // bright white
+        ]
+    },
+    {
+        name: 'Synthwave 84',
+        author: 'Retro Neon',
+        colors: [
+            '#241b2f', // background
+            '#fe4450', // red
+            '#72f1b8', // green
+            '#fede5d', // yellow
+            '#03edf9', // blue
+            '#ff7edb', // magenta
+            '#36f9f6', // cyan
+            '#f7f7f7', // foreground
+            '#495495', // bright black
+            '#fe4450', // bright red
+            '#72f1b8', // bright green
+            '#fede5d', // bright yellow
+            '#03edf9', // bright blue
+            '#ff7edb', // bright magenta
+            '#36f9f6', // bright cyan
+            '#ffffff', // bright white
+        ]
+    },
+    // Light Themes
+    {
+        name: 'Solarized Light',
+        author: 'Precision Colors',
+        colors: [
+            '#fdf6e3', // background
+            '#dc322f', // red
+            '#859900', // green
+            '#b58900', // yellow
+            '#268bd2', // blue
+            '#d33682', // magenta
+            '#2aa198', // cyan
+            '#657b83', // foreground
+            '#eee8d5', // bright black
+            '#cb4b16', // bright red
+            '#93a1a1', // bright green
+            '#839496', // bright yellow
+            '#657b83', // bright blue
+            '#6c71c4', // bright magenta
+            '#586e75', // bright cyan
+            '#073642', // bright white
+        ]
+    },
+    {
+        name: 'Gruvbox Light',
+        author: 'Retro Groove',
+        colors: [
+            '#fbf1c7', // background
+            '#cc241d', // red
+            '#98971a', // green
+            '#d79921', // yellow
+            '#458588', // blue
+            '#b16286', // magenta
+            '#689d6a', // cyan
+            '#3c3836', // foreground
+            '#ebdbb2', // bright black
+            '#9d0006', // bright red
+            '#79740e', // bright green
+            '#b57614', // bright yellow
+            '#076678', // bright blue
+            '#8f3f71', // bright magenta
+            '#427b58', // bright cyan
+            '#282828', // bright white
+        ]
+    },
+    {
+        name: 'Catppuccin Latte',
+        author: 'Soothing Pastel',
+        colors: [
+            '#eff1f5', // background
+            '#d20f39', // red
+            '#40a02b', // green
+            '#df8e1d', // yellow
+            '#1e66f5', // blue
+            '#ea76cb', // magenta
+            '#179299', // cyan
+            '#4c4f69', // foreground
+            '#ccd0da', // bright black
+            '#d20f39', // bright red
+            '#40a02b', // bright green
+            '#df8e1d', // bright yellow
+            '#1e66f5', // bright blue
+            '#ea76cb', // bright magenta
+            '#179299', // bright cyan
+            '#5c5f77', // bright white
+        ]
+    },
+    {
+        name: 'One Light',
+        author: 'Atom Inspired',
+        colors: [
+            '#fafafa', // background
+            '#e45649', // red
+            '#50a14f', // green
+            '#c18401', // yellow
+            '#4078f2', // blue
+            '#a626a4', // magenta
+            '#0184bc', // cyan
+            '#383a42', // foreground
+            '#e5e5e6', // bright black
+            '#e45649', // bright red
+            '#50a14f', // bright green
+            '#c18401', // bright yellow
+            '#4078f2', // bright blue
+            '#a626a4', // bright magenta
+            '#0184bc', // bright cyan
+            '#090a0b', // bright white
+        ]
+    },
+    {
+        name: 'Rose Pine Dawn',
+        author: 'Rosé Pine',
+        colors: [
+            '#faf4ed', // background
+            '#b4637a', // red
+            '#56949f', // green
+            '#ea9d34', // yellow
+            '#286983', // blue
+            '#907aa9', // magenta
+            '#d7827e', // cyan
+            '#575279', // foreground
+            '#fffaf3', // bright black
+            '#b4637a', // bright red
+            '#56949f', // bright green
+            '#ea9d34', // bright yellow
+            '#286983', // bright blue
+            '#907aa9', // bright magenta
+            '#d7827e', // bright cyan
+            '#575279', // bright white
+        ]
+    },
+    {
+        name: 'Everforest Light',
+        author: 'Nature Inspired',
+        colors: [
+            '#fdf6e3', // background
+            '#f85552', // red
+            '#8da101', // green
+            '#dfa000', // yellow
+            '#3a94c5', // blue
+            '#df69ba', // magenta
+            '#35a77c', // cyan
+            '#5c6a72', // foreground
+            '#f0f0f0', // bright black
+            '#f85552', // bright red
+            '#8da101', // bright green
+            '#dfa000', // bright yellow
+            '#3a94c5', // bright blue
+            '#df69ba', // bright magenta
+            '#35a77c', // bright cyan
+            '#5c6a72', // bright white
+        ]
+    },
+    {
+        name: 'Tokyo Night Day',
+        author: 'Day Coding',
+        colors: [
+            '#d5d6db', // background
+            '#f52a65', // red
+            '#587539', // green
+            '#8c6c3e', // yellow
+            '#2e7de9', // blue
+            '#9854f1', // magenta
+            '#007197', // cyan
+            '#3760bf', // foreground
+            '#9699a3', // bright black
+            '#f52a65', // bright red
+            '#587539', // bright green
+            '#8c6c3e', // bright yellow
+            '#2e7de9', // bright blue
+            '#9854f1', // bright magenta
+            '#007197', // bright cyan
+            '#3760bf', // bright white
+        ]
+    },
+    {
+        name: 'GitHub Dark',
+        author: 'GitHub',
+        colors: [
+            '#0d1117', // background
+            '#ff7b72', // red
+            '#3fb950', // green
+            '#d29922', // yellow
+            '#58a6ff', // blue
+            '#bc8cff', // magenta
+            '#39c5cf', // cyan
+            '#b1bac4', // foreground
+            '#484f58', // bright black
+            '#ffa198', // bright red
+            '#56d364', // bright green
+            '#e3b341', // bright yellow
+            '#79c0ff', // bright blue
+            '#d2a8ff', // bright magenta
+            '#56d4dd', // bright cyan
+            '#f0f6fc', // bright white
+        ]
+    },
+    {
+        name: 'GitHub Light',
+        author: 'GitHub',
+        colors: [
+            '#ffffff', // background
+            '#cf222e', // red
+            '#116329', // green
+            '#4d2d00', // yellow
+            '#0969da', // blue
+            '#8250df', // magenta
+            '#1b7c83', // cyan
+            '#24292f', // foreground
+            '#f6f8fa', // bright black
+            '#a40e26', // bright red
+            '#1a7f37', // bright green
+            '#633c01', // bright yellow
+            '#0550ae', // bright blue
+            '#8250df', // bright magenta
+            '#1b7c83', // bright cyan
+            '#1f2328', // bright white
+        ]
+    },
+    {
+        name: 'Monochrome Dark',
+        author: 'Pure Minimalism',
+        colors: [
+            '#000000', // background
+            '#7c7c7c', // red (dark gray)
+            '#8b8b8b', // green (medium gray)
+            '#a0a0a0', // yellow (light gray)
+            '#686868', // blue (darker gray)
+            '#747474', // magenta (medium dark gray)
+            '#868686', // cyan (medium gray)
+            '#b9b9b9', // foreground (light gray)
+            '#525252', // bright black (dark gray)
+            '#7c7c7c', // bright red
+            '#8b8b8b', // bright green
+            '#a0a0a0', // bright yellow
+            '#686868', // bright blue
+            '#747474', // bright magenta
+            '#868686', // bright cyan
+            '#ffffff', // bright white
+        ]
+    },
+    {
+        name: 'Monochrome Light',
+        author: 'Pure Minimalism',
+        colors: [
+            '#ffffff', // background
+            '#5a5a5a', // red (medium gray)
+            '#6e6e6e', // green (medium-light gray)
+            '#808080', // yellow (gray)
+            '#4a4a4a', // blue (darker gray)
+            '#5e5e5e', // magenta (medium-dark gray)
+            '#707070', // cyan (medium gray)
+            '#1a1a1a', // foreground (very dark gray)
+            '#d4d4d4', // bright black (light gray)
+            '#5a5a5a', // bright red
+            '#6e6e6e', // bright green
+            '#808080', // bright yellow
+            '#4a4a4a', // bright blue
+            '#5e5e5e', // bright magenta
+            '#707070', // bright cyan
+            '#000000', // bright white (black)
+        ]
+    },
 ];
